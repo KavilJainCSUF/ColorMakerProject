12c132b256318fda12f07b4b0367a97da4a70db8
KavilJainCSUF <kavil.jain@csu.fullerton.edu>
2023-02-18 17:20:00 -0800

    Initial commit

---
+++ app/src/main/java/edu/fullerton/kj/colormakerproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.fullerton.kj.colormakerproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
